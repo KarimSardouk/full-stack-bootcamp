bc512e6a215af8f2efc41dfb8d55c6b71b87b599
Karim Sardouk <karimsardouk727@gmail.com>
2023-08-17 12:19:12 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,5 +1,17 @@
 console.log('I am linked now!');
-let name ="Karim";
+let name = "Karim";
 function myFunction() {
     alert(name);
-  }
+}
+var age1 = 22;
+var lastName1 = "Sardouk";
+var DOB1 = "27-7-2001";
+let age2 = 22;
+let lastName2 = "Sardouk";
+let DOB2 = "27-7-2001";
+const age3 = 22;
+const lastName3 = "Sardouk";
+const DOB3 = "27-7-2001";
+console.log(age1, lastName1, DOB1);
+console.log(age2, lastName2, DOB2);
+console.log(age3, lastName3, DOB3);
