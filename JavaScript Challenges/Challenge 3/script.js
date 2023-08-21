7b2f2896cccaac879b90e92084ac003e9dbf2bd3
Karim Sardouk <karimsardouk727@gmail.com>
2023-08-21 15:53:04 +0300

    Challenge 3 - Done with step 2

--- JavaScript Challenges/Challenge 3/script.js
+++ JavaScript Challenges/Challenge 3/script.js
@@ -56,4 +56,28 @@ function compute(num0, num6, operation) {
 
 // Example usage:
 const result0 = compute(14, 27, "add");
-console.log(result0); // This will log the result of adding 14 and 27
+console.log(result0); // This will log the result of 41, adding 14 and 27
+
+const squareNumber = (number) => number * number;
+
+// Example usage:
+const result11 = squareNumber(4);
+console.log(result11); // This will log the square of 5, which is 25
+
+// Global scope
+let globalVar = 10; // This variable is in the global scope
+
+const squareNumber1 = (number) => {
+    // Local scope
+    let localVar = 20; // This variable is in the local scope of the squareNumber function
+    console.log("Inside squareNumber function:");
+    console.log("globalVar:", globalVar); // Accessing the global variable from the local scope
+    console.log("localVar:", localVar);
+};
+
+// Call the function
+squareNumber1(5);
+
+console.log("Outside squareNumber function:");
+console.log("globalVar:", globalVar); // Accessing the global variable from the global scope
+// Attempting to access localVar from the global scope will result in an error
