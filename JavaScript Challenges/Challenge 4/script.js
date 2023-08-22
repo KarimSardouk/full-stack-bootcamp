03ee0ab7225508e8eedca7e3fb863176f55adc6b
Karim Sardouk <karimsardouk727@gmail.com>
2023-08-22 16:02:44 +0300

    Challenge 4 - Done with step 3

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -1,5 +1,7 @@
+//Step 1: Arrays and Objects
 let fiveNumbers = [11,22,33,44,77];
 const personalInfo={name1:"Karim", age:22, height:1.75, location:"Beirut", nationality:"Lebanese"};
+//Step 2: Spread Operator
 //New Array with spread
 const nextThreenumbers=[1,2,3];
 const allNumbers=[...fiveNumbers,...nextThreenumbers];
@@ -8,3 +10,10 @@ console.log(allNumbers);
 const additionalInfo={ occupation:"programmer", hobby:"going to the gym",education:"USJ, majored in Business Management"};
 const fullInfo={...personalInfo,additionalInfo};
 console.log(fullInfo);
+//Step 3: Destructuring
+//Destructuring Arrays
+const [num1,num2,num3]=fiveNumbers;
+//Destructuring Objects
+// Extract properties into variables
+const { name1, location,nationality } = personalInfo;
+
