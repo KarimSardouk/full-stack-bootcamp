161a438f5340a32443a733e7fe0054b062502f2c
Karim Sardouk <karimsardouk727@gmail.com>
2023-08-22 15:18:59 +0300

    Challenge 4 - Done with step 2

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -1,2 +1,10 @@
 let fiveNumbers = [11,22,33,44,77];
 const personalInfo={name1:"Karim", age:22, height:1.75, location:"Beirut", nationality:"Lebanese"};
+//New Array with spread
+const nextThreenumbers=[1,2,3];
+const allNumbers=[...fiveNumbers,...nextThreenumbers];
+console.log(allNumbers);
+//New Object with Spread
+const additionalInfo={ occupation:"programmer", hobby:"going to the gym",education:"USJ, majored in Business Management"};
+const fullInfo={...personalInfo,additionalInfo};
+console.log(fullInfo);
