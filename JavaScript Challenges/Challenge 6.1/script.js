c3484383a49aab72b1039e568712054dd2f67e64
Karim Sardouk <karimsardouk727@gmail.com>
2023-08-29 10:17:31 +0300

    Challenge 6.1 - Done with Step 2, a couple of modifications were done

--- JavaScript Challenges/Challenge 6.1/script.js
+++ JavaScript Challenges/Challenge 6.1/script.js
@@ -11,9 +11,10 @@ function delay(ms) {
 fetch('https://uselessfacts.jsph.pl/random.json?language=en')
     .then(response => response.json())
     .then(postData => {
-        console.log(postData);
+        console.log(postData.text);
     })
     .catch(error => {
         console.error(`Error fetching post ${post}:`, error);
     });
-    
+//Step 3: Rewrite with Async/Await
+//Using async/await, rewrite the previous challenge (fetching a random fact) to achieve the same result
