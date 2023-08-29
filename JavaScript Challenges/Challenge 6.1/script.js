a08115aee6ca03841ddba62aa11bf4a43591a033
Karim Sardouk <karimsardouk727@gmail.com>
2023-08-29 10:13:00 +0300

    Challenge 6.1 - Done with Step 2

--- JavaScript Challenges/Challenge 6.1/script.js
+++ JavaScript Challenges/Challenge 6.1/script.js
@@ -7,3 +7,13 @@ function delay(ms) {
   delay(2000).then(() => {
     console.log('Promise resolved after 2 seconds');
   });
+//Step 2: Fetch a random fact 
+fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+    .then(response => response.json())
+    .then(postData => {
+        console.log(postData);
+    })
+    .catch(error => {
+        console.error(`Error fetching post ${post}:`, error);
+    });
+    
